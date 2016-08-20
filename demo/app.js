40b7d360e9f7cc059610e3210622ff2967353794
Omer Dobzinsky <omerdob@gmail.com>
2016-08-20 17:27:47 +0300

    * Created a package.json for the project * Using hardcoded settings in demo app.js * Added an index page to the demo

--- demo/app.js
+++ demo/app.js
@@ -7,6 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(serverConfig.port, 'localhost', () => {
-    console.log(`Server started at http://${serverConfig.hostname}:${serverConfig.port}`);
+app.listen(3000, 'localhost', () => {
+    console.log(`Server started at http://localhost:3000`);
 });
