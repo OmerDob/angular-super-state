4f8e29c41d64937ad7f7bdbaebbb5ead0880ce88
Omer Dobzinsky <omerdob@gmail.com>
2016-08-20 19:31:47 +0300

    * Fixed demo app node service

--- demo/app.js
+++ demo/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'bin')));
 
 app.listen(3000, 'localhost', () => {
     console.log(`Server started at http://localhost:3000`);
